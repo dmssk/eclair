4f7341b4dd6a06465b756123af454fd977f3216b
Dmytro Puzyrenko <myaczt@gmail.com>
2020-12-16 22:36:47 +0200

    add calculations

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,26 @@
-import { qwe } from './test';
-
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
 
 
-console.log(qwe);
+$(window).on('load', function(e) {
+  
+
+  $('.item__actions').on('click', function(e) {
+    let num = +$(this).find('.item__quantity-text').text()
+    
+    if ($(e.target).hasClass('item__btn--minus') && num > 1) {
+      $(this).find('.item__quantity-text').text(num - 1)
+    }
+    
+    if ($(e.target).hasClass('item__btn--plus')) {
+      $(this).find('.item__quantity-text').text(num + 1)
+    }
+    
+    if ($(e.target).hasClass('btn')) {
+      e.preventDefault()
+      $(this).find('.item__quantity-text').text(1)
+    }
+
+  })
+})
--- src/js/test.js
+++
@@ -1,2 +0,0 @@
-
-export const qwe = '123'
