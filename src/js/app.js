9b55a0eb5fb4701f409a1a4b0f3cffb561e00090
Dmytro Puzyrenko <myaczt@gmail.com>
2020-12-17 22:19:38 +0200

    add card page

--- src/js/app.js
+++ src/js/app.js
@@ -2,46 +2,63 @@ import $ from "jquery";
 import 'slick-carousel'
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
 
 
-$(window).on('load', function(e) {
 
-  function scrollHandler () {
+// eslint-disable-next-line no-undef
+$(window).on('load', (e) => {
+  function scrollHandler() {
+    // eslint-disable-next-line no-undef
     if ($(window).scrollTop() > 400) {
-      $('.header__bottom.fixed').addClass('active')
+      $('.header__bottom.fixed').addClass('active');
     } else {
-      $('.header__bottom.fixed').removeClass('active')
+      $('.header__bottom.fixed').removeClass('active');
     }
   }
-  scrollHandler()
-
-  $(window).on('scroll', function() {
-    scrollHandler()
-  })
+  scrollHandler();
+  // eslint-disable-next-line no-undef
+  $(window).on('scroll', () => {
+    scrollHandler();
+  });
 
 
   $('.carousel').slick({
     infinite: true,
     prevArrow: $('.buttons .prev'),
-    nextArrow: $('.buttons .next')
-  })
-  $('.item__actions').on('click', function(e) {
-    let num = +$(this).find('.item__quantity-text').text()
-    
+    nextArrow: $('.buttons .next'),
+  });
+
+  $('.items-switch__top').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 1,
+    asNavFor: $('.items-switch__bottom'),
+  });
+
+  $('.items-switch__bottom').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 6,
+    focusOnSelect: true,
+    asNavFor: $('.items-switch__top'),
+  });
+
+  $('.item__actions').on('click', function actionItemsClickHandler(e) {
+    const num = +$(this).find('.item__quantity-text').text()
+
     if ($(e.target).hasClass('item__btn--minus') && num > 1) {
-      $(this).find('.item__quantity-text').text(num - 1)
-    }
-    
-    if ($(e.target).hasClass('item__btn--plus')) {
-      $(this).find('.item__quantity-text').text(num + 1)
-    }
-    
-    if ($(e.target).hasClass('btn')) {
-      e.preventDefault()
-      $(this).find('.item__quantity-text').text(1)
+      $(this).find('.item__quantity-text').text(num - 1);
     }
 
-  })
-})
+    if ($(e.target).hasClass('item__btn--plus')) {
+      $(this).find('.item__quantity-text').text(num + 1);
+    }
+
+    if ($(e.target).hasClass('btn')) {
+      e.preventDefault();
+      $(this).find('.item__quantity-text').text(1);
+    }
+  });
+});
